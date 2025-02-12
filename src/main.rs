988f8f279baef3ff75076de7e13dfdfed910cdde
krypton-0x00 <shakirgulzar0x00@gmail.com>
2025-02-13 03:03:54 +0530

    FIX: DYANMIC PATH

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use krtodo::start_tui;
+use std::process::Command;
 
 fn main() {
-    let db_path = "/home/nesu/todos.csv";
-    if let Err(err) = start_tui(db_path) {
+    let cmd = Command::new("sh")
+        .arg("-c")
+        .arg("whoami")
+        .output()
+        .expect("Error while getting user.");
+
+    let mut user = String::from_utf8(cmd.stdout).unwrap();
+    user.pop();
+    let db_path = format!("/home/{}/todos.csv", user);
+    if let Err(err) = start_tui(&db_path) {
         eprintln!("Error running TUI: {}", err);
     }
 }
